1e35e52c190dfad2ff0edfe675e68616c9104ea6
AlexPasechnuy <alexpasechnuy@gmail.com>
2020-05-24 14:51:43 +0300

    Now lab3 changed for suregit add .!

--- Lab3/Task_8.py
+++ Lab3/Task_8.py
@@ -4,18 +4,14 @@ import scipy.stats
 
 def matrix_normal_logpdf(X, m, U, V):
     return scipy.stats.matrix_normal.logpdf\
-        (X, mean=m, rowcov=U, colcov=V).round(SIGN_TO_ROUND)
+        (X, mean=m, rowcov=U, colcov=V).round(3)
 
 
 if __name__ == '__main__':
-    SIGN_TO_ROUND = 3
-    N = 5
-    D = 4
-
-    m = np.random.uniform(-1, 1, size=N * D). \
-        reshape(N, D).round(SIGN_TO_ROUND)
-    U = np.diag([x for x in range(1, N + 1)]).round(SIGN_TO_ROUND)
-    V = 0.3 * np.identity(D).round(SIGN_TO_ROUND)
+    m = np.random.uniform(-1, 1, size=5 * 4). \
+        reshape(5, 4).round(3)
+    U = np.diag([x for x in range(1, 5 + 1)]).round(3)
+    V = 0.3 * np.identity(4).round(3)
     X = m + 0.1
 
     print(m)
